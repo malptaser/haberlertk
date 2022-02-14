e992b9daa171223014ae6caf638e67b1ec637255
M.Alp <mtaser@burgan.com.tr>
2022-02-14 13:01:39 +0300

    source tree deneme

--- HaberlerBB/NewsTableViewController.swift
+++ HaberlerBB/NewsTableViewController.swift
@@ -4,12 +4,17 @@
 //
 //  Created by BB-MP on 11.02.2022.
 //
-
 import UIKit
 
 class NewsTableViewController: UITableViewController {
-
+    
+    // MARK: - Functions
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return NewsData.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            return tableView.dequeueReusableCell(withIdentifier: "NewsCell", for: indexPath)
+   
     }
 }
